29526806ee3de7842f34987597bbd07162a84505
codingpioneer <60292577+doopyo85@users.noreply.github.com>
2024-08-24 02:24:43 +0900

    Update session.js
    이 수정된 코드는 다음과 같은 변경사항을 포함합니다:
    
    sessionInitialState를 export합니다.
    리듀서 함수를 default export로 내보냅니다.
    setSessionData 액션 생성자를 named export로 내보냅니다.

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -1,22 +1,25 @@
-const initialState = {
-  user: null
+// Initial state
+export const sessionInitialState = {
+    user: null
 };
 
-const sessionReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'SET_SESSION_DATA':
-      return {
-        ...state,
-        user: action.payload
-      };
-    default:
-      return state;
-  }
+// Reducer
+const sessionReducer = (state = sessionInitialState, action) => {
+    switch (action.type) {
+        case 'SET_SESSION_DATA':
+            return {
+                ...state,
+                user: action.payload
+            };
+        default:
+            return state;
+    }
 };
 
+// Action creator
 export const setSessionData = (sessionData) => ({
-  type: 'SET_SESSION_DATA',
-  payload: sessionData
+    type: 'SET_SESSION_DATA',
+    payload: sessionData
 });
 
 export default sessionReducer;
