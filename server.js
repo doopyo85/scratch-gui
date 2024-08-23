be786af06476fee338298967fc03a8d74ac3fcbe
Ubuntu <ubuntu@ip-172-31-0-184.ap-northeast-2.compute.internal>
2024-08-23 15:55:37 +0000

    현재 상태를 main 분기로 저장

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'build')));
+
+// 모든 요청을 index.html로 라우팅 (SPA를 위해)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8601;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
